05422a080fcd1f1e13e63418cb9cb575162726be
Derya Tanriverdi <deryatanriverdi_@hotmail.com>
2019-07-08 23:00:22 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,5 @@
+board = [" ", " ", "  ", "  ", "  ", "  ", "  "]
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -5,7 +7,5 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
-board = [" ", " ", "  ", "  ", "  ", "  ", "  "]
 
 puts display_board(board)
